338a2692aed77665e57461d43b34fe9d2139139e
Tryanks <tryanks@outlook.com>
2023-04-18 10:15:59 +0800

    Update version

--- snbtlib/__init__.py
+++ snbtlib/__init__.py
@@ -1,1 +1,18 @@
-from snbtlib.formatter import dumps, loads
+from snbtlib.formatter import dumps as d, loads as l
+
+
+def dump(obj, fp, **kwargs):
+    fp.write(d(obj, **kwargs))
+
+
+def dumps(obj, **kwargs):
+    return d(obj, **kwargs)
+
+
+def load(fp, **kwargs):
+    return l(fp.read(), **kwargs)
+
+
+def loads(s, **kwargs):
+    return l(s, **kwargs)
+
