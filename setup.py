66402e6be264729613db2ee1fded94346cb7600a
Tryanks <tryanks@outlook.com>
2023-05-24 09:40:37 +0800

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='snbtlib',
-    version='0.0.7',
+    version='0.1.0',
     keywords='minecraft',
     description='a formatter for snbt from minecraft',
     long_description=long_description,
